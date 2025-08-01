8d30cef2a2ce066b4a9045c686ee9aea64a408bd
R Ganesh <68594076+rganesh203@users.noreply.github.com>
2025-08-01 09:55:34 +0530

    Committing 2 items from workspace 71b4a314-d6aa-4a9f-8d58-212ca6ec7ace

---
+++ WH_Table.Warehouse/dbo/Tables/RegionalOrdersData.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[RegionalOrdersData] (
+
+	[OrderID] varchar(8000) NULL, 
+	[CustomerID] varchar(8000) NULL, 
+	[OrderDate] varchar(8000) NULL, 
+	[Testing] varchar(8000) NULL
+);
