f34ba3d3e6a85033debbae6613deaed126e73f0f
R Ganesh <68594076+rganesh203@users.noreply.github.com>
2025-08-03 19:45:28 +0530

    Committing 2 items from workspace 71b4a314-d6aa-4a9f-8d58-212ca6ec7ace

---
+++ WH_Table.Warehouse/dbo/Tables/month_20250803_141116.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[month_20250803_141116] (
+
+	[Name] varchar(8000) NULL, 
+	[Abbreviation] varchar(8000) NULL, 
+	[Numeric] varchar(8000) NULL, 
+	[Numeric-2] varchar(8000) NULL
+);
