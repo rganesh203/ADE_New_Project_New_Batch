30d7822d726ddb4412c457060ce90602c7034f58
R Ganesh <68594076+rganesh203@users.noreply.github.com>
2025-09-03 17:56:42 +0530

    Committing 1 item from workspace 71b4a314-d6aa-4a9f-8d58-212ca6ec7ace

---
+++ Which Lakehouse Is My Notebook Using&#63; How to Switch Lakehouses.Notebook/notebook-content.py
@@ -0,0 +1,40 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "99ea534f-c37f-442b-bede-d6fe937740c4",
+# META       "default_lakehouse_name": "Files_LH",
+# META       "default_lakehouse_workspace_id": "0fcd34f1-88ff-470f-9971-2066f6203f0a",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f0e063ed-797d-4cc3-b4d2-7d1438570332"
+# META         },
+# META         {
+# META           "id": "99ea534f-c37f-442b-bede-d6fe937740c4"
+# META         },
+# META         {
+# META           "id": "92569736-95ce-4c0b-b3e6-b591c0826099"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC show tables
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
