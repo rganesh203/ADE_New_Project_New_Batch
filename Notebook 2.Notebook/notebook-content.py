63dba169a603bfc19d103a81b9b2ceaff105736f
R Ganesh <68594076+rganesh203@users.noreply.github.com>
2025-09-20 21:07:35 +0530

    Committing 1 item from workspace 71b4a314-d6aa-4a9f-8d58-212ca6ec7ace

---
+++ Notebook 2.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "4ea02a8a-4ba6-4965-939c-3a720db3ba47",
+# META       "default_lakehouse_name": "Lakehouse_for_files",
+# META       "default_lakehouse_workspace_id": "71b4a314-d6aa-4a9f-8d58-212ca6ec7ace",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "4ea02a8a-4ba6-4965-939c-3a720db3ba47"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load("Files/dept.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/dept.csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
